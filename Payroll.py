efc8f89b7f72980433a37bdc592deddc838bb91c
Jake <Jake.Eigsti@du.edu>
2024-05-06 13:26:15 -0600

    initial

---
+++ Payroll.py
@@ -0,0 +1,15 @@
+from Employee import Employee
+
+class Payroll():
+    def __init__(self):
+        self.emp = []
+
+    def __repr__(self):
+        return str(self.emp)
+    
+    def addEmp(self, e):
+        self.emp.append(e)
+
+
+
+    
